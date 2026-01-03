6d75e79d9fcde6f605c0b0986888a41ff8b0ce8b
dozapsixoza1 <166742425+dozapsixoza1@users.noreply.github.com>
2026-01-03 21:35:08 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+bot_token = '8507316166:AAEjZ1mcsaMSJKoSMd5k-yEPQgaRFNk8EV4'
+admin_id = [7504103313]
+channel_id = -1003262419682
+channel2_id = -1003527789520
---
+++ keyboard.py
@@ -0,0 +1,23 @@
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardButton as ib
+kbadmin = ReplyKeyboardMarkup(resize_keyboard=True)
+kbadmin.row("📟Статистика","✉️Рассылка")
+
+kbsend = ReplyKeyboardMarkup(resize_keyboard=True)
+kbsend.row("С фото","Без фото"," 🐍Назад")
+
+kbsub = InlineKeyboardMarkup()
+kbsub.row(ib('〽️ Подписаться 1', url='https://t.me/+HlvzFyd2AakyZGRi'))
+kbsub.row(ib('〽️ Подписаться 2', url='https://t.me/+cbnStFW4c1M5OWZi'))
+kbsub.row(ib('✅ Подписался', callback_data='subbed'))
+
+kbmenu = InlineKeyboardMarkup()
+kbmenu.add(ib('IP-адрес', callback_data='checkip'))
+kbmenu.add(ib('BTC-адрес', callback_data='checkbtc'))
+kbmenu.add(ib('MAC-адрес', callback_data='checkmac'))
+kbmenu.add(ib('Никнейм', callback_data='checknick'))
+kbmenu.add(ib('Номер телефона', callback_data='checknum'))
+kbmenu.add(ib('BIN карты', callback_data='checkbin'))
+kbmenu.add(ib('VK профиль', callback_data='checkvk'))
+
+kbback = InlineKeyboardMarkup().row(ib('Назад', callback_data='back'))
